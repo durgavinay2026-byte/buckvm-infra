90f1e0593f94eb618d556541ed53137c9a0ceb83
Vinay Durga <durgavinay143@gmail.com>
2025-12-17 22:30:29 -0500

    little changes

--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
-variable "vm_name" { type = string, default = "hello-vm" }
-variable "vm_machine_type" { type = string, default = "e2-medium" }
-variable "create_kms" { type = bool, default = true }
-variable "tags" { type = list(string), default = ["http-server"] }
+variable "vm_name" {
+	type    = string
+	default = "hello-vm"
+}
+
+variable "vm_machine_type" {
+	type    = string
+	default = "e2-medium"
+}
+
+variable "create_kms" {
+	type    = bool
+	default = true
+}
+
+variable "tags" {
+	type    = list(string)
+	default = ["http-server"]
+}
