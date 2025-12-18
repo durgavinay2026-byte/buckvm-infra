5bfb0970a10cf32bcb4f23c71ea5b79175c08467
Vinay Durga <durgavinay143@gmail.com>
2025-12-17 22:19:16 -0500

    minmal changes

--- providers.tf
+++ providers.tf
@@ -1,6 +1,17 @@
-variable "project_id" { type = string, default = "buckvm" }
-variable "region"     { type = string, default = "us-central1" }
-variable "zone"       { type = string, default = "us-central1-a" }
+variable "project_id" {
+  type    = string
+  default = "buckvm"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
 
 provider "google" {
   project = var.project_id
